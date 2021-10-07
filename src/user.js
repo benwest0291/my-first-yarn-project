b51d397ab5a4b52976c316eddd8d79f7d0bc4228
Ben West <benwest0291@outlook.com>
2021-10-07 19:10:06 +0100

    added JS code

---
+++ src/user.js
@@ -0,0 +1,13 @@
+function greetUser(name) {
+  console.log(`Hello ${name}`);
+  console.log("hello from ben");
+}
+
+greetUser("anonymous");
+
+function greetUserTwo(name) {
+  console.log(`Hello ${name}`);
+}
+
+greetUser("anonymous");
+greetUserTwo("anonymous");
