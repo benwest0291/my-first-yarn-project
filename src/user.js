1d38b8e6be1e54189b71e83957be0dff163b9f9c
Ben West <benwest0291@outlook.com>
2021-10-08 12:53:45 +0100

    added j jquery

--- src/user.js
+++ src/user.js
@@ -1,8 +1,14 @@
 // ./src/user.js
+import $ from "jquery";
 import { capitalize } from "./helpers";
+
 
 function greetUser(name) {
   console.log(`Hello ${capitalize(name)}`);
 }
 
 greetUser("anonymous");
+
+$(document).ready(function () {
+  $("#msgid").html("This is Hello World by JQuery");
+});
